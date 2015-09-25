2512041668b83a633644b63da39acc838fd4fb04
Jérôme Avoustin <jerome@rehia.fr>
2015-09-25 23:36:52 +0200

    build ze wall & tower.

--- src/main/Cell.jsx
+++ src/main/Cell.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  render: function(){
-    return <li className="cell"></li>;
+
+  render: function() {
+    var stateRendered = null;
+    if (this.state.type === 'wall') {
+      stateRendered = <img src='img/wall.png' />;
+    } else if (this.state.type === 'tower') {
+      stateRendered = <img src='img/tower.png' />;
+    }
+    return <li className="cell">{stateRendered}</li>;
+  },
+
+  getInitialState: function () {
+    return { type : 'grass' };
   }
+
 });
