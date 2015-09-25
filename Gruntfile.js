c66d8abc20c2e8d2271ff7692437edccf98e84da
Franck Ernewein <franck.ernewein@gmail.com>
2015-09-25 15:31:44 +0200

    bootstrap project

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    browserify: {
+      options: {
+        transform: ['reactify'],
+        watch: true,
+        browserifyOptions: {
+          debug: true,
+        }
+      },
+      main: {
+        src: 'src/main/index.js',
+        dest: 'public/js/main.js'
+      }
+    },
+    watch: {}
+  });
+
+  grunt.registerTask('dev', ['browserify', 'watch']);
+  grunt.registerTask('default', ['browserify']);
+
+};
